c2ab601b62e8a9d586dd828007f4d39a0e300365
Chris Brookes <dev@net.com>
2020-04-23 22:10:52 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='robotframework-remoterunner',
-    version='1.1.0rc1',
+    version='1.1.0',
     author='Chris Brookes',
     author_email='chris-brookes93@outlook.com',
     description='A library that provides the ability to execute RobotFramework test suites on a remote host. This comes'
